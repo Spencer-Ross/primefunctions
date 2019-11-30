1e9b697979a154628ef000555f6a0a9590db71ee
Spencer <spencerthomasr@gmail.com>
2019-11-30 14:26:51 -0800

    cumulativeSum tests are working and succeeding correctly

--- testCorrectness.js
+++ testCorrectness.js
@@ -20,12 +20,12 @@ describe('PrimeFunctions', function () {
   });
   describe('cumulativeSum()', function () {
     it('should return [1, 3, 6, 10] for testArr1', function () {
-      chai.expect(cumulativeSum(testArr1)).to.equal([1, 3, 6, 10]);
+      chai.expect(cumulativeSum(testArr1)).to.eql([1, 3, 6, 10]);
     });
   });
   describe('cumulativeSum()', function () {
     it('should return [1, 3, 6, 10] for testArr2', function () {
-      chai.expect(cumulativeSum(testArr2)).to.equal([10, 21, 33, 46, 60]);
+      chai.expect(cumulativeSum(testArr2)).to.eql([10, 21, 33, 46, 60]);
     });
   });
   describe('maxPrimeSum()', function () {
