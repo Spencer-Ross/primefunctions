b01d9f3da9b0e633333c02569d0b9cdccace57f5
Spencer <spencerthomasr@gmail.com>
2019-12-01 16:27:07 -0800

    Got maxPrimeSum Test to work correctly Created primefunctions2 to make optimization changes Created new performance test for maxPrimeSum for 10000 at <20ms It failed on first attempt

---
+++ testPerformance.js
@@ -0,0 +1,8 @@
+describe('PrimeFunctions2', function () {
+  describe('maxPrimeSum()', function () {
+    it('should take less than 20ms', function () {
+      this.slow(20);
+      chai.expect(maxPrimeSum(10000)).to.eql([ 9521, 65 ]);
+    });
+  });
+});
